fe5d4942b069ebd03358cbaffeb2da080c666d3d
Michael Scott Weber <mscottweber@gmail.com>
2025-03-18 06:45:56 -0500

    Adds Inter font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <main className='p-4 sm:p-6 md:p-8 flex min-h-screen bg-white w-screen'>
+    <main className='p-4 sm:p-6 md:p-8 flex min-h-screen bg-white w-screen font-display'>
       <div className='border-0 md:border border-gray-300 flex flex-col md:flex-row rounded w-full'>
         <Sidebar />
         <section>
