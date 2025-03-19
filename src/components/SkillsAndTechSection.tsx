523bd4b84406ae4d55d831f64ddf4d012b380805
Michael Scott Weber <mscottweber@gmail.com>
2025-03-19 17:32:14 -0500

    More styling updates, and adds content

--- src/components/SkillsAndTechSection.tsx
+++ src/components/SkillsAndTechSection.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import SearchPill from './SearchPill';
+import SkillPill from './SkillPill';
+import CheckSquare from './icons/CheckSquare';
+import Layout from './icons/Layout';
+import PenTool from './icons/PenTool';
+import Share from './icons/Share';
+import BookOpen from './icons/BookOpen';
 
 type Skill = {
   name: string;
@@ -43,7 +49,11 @@ function SkillsAndTechSection({ data }: SkillsAndTechSectionProps) {
   };
 
   const renderSkillsList = filterSkills(data.skills).map((skill) => {
-    return <li key={skill.name}>{skill.name}</li>;
+    return (
+      <li key={skill.name}>
+        <SkillPill skill={skill.name} />
+      </li>
+    );
   });
 
   return (
@@ -51,8 +61,53 @@ function SkillsAndTechSection({ data }: SkillsAndTechSectionProps) {
       <h2 className='text-xl text-black font-medium mb-4 md:mb-7'>
         Skills and Tech
       </h2>
-      <ul className='flex flex-wrap gap-2 mb-4'>{renderSearchTermList}</ul>
-      <ul className='flex flex-wrap gap-2'>{renderSkillsList}</ul>
+      <ul className='flex flex-wrap gap-2 mb-5'>{renderSearchTermList}</ul>
+      <ul className='flex flex-wrap gap-3 mb-8'>{renderSkillsList}</ul>
+      <div className='mb-6'>
+        <h3 className='text-black text-lg font-medium flex mb-2'>
+          <CheckSquare classNames='w-6 mr-2' /> Accessibility
+        </h3>
+        <p className='text-black text-base'>
+          Experienced in semantic HTML structure, screen readers, and in writing
+          code that is WCAG 2.1 compliant.
+        </p>
+      </div>
+      <div className='mb-6'>
+        <h3 className='text-black text-lg font-medium flex mb-2'>
+          <Layout classNames='w-6 mr-2' /> Responsive Design
+        </h3>
+        <p className='text-black text-base'>
+          Projects that look good on all devices and all browsers. If it doesn't
+          work on mobile, then it doesn't work.
+        </p>
+      </div>
+      <div className='mb-6'>
+        <h3 className='text-black text-lg font-medium flex mb-2'>
+          <PenTool classNames='w-6 mr-2' /> Image Design
+        </h3>
+        <p className='text-black text-base'>
+          A keen eye for design, and skilled with Adobe Photoshop, Illustrator,
+          and Figma.
+        </p>
+      </div>
+      <div className='mb-6'>
+        <h3 className='text-black text-lg font-medium flex mb-2'>
+          <Share classNames='w-6 mr-2' /> Collaboration
+        </h3>
+        <p className='text-black text-base'>
+          Experienced working onsite and remotely in cross-disceplanary teams.
+          Excellent communication skills.
+        </p>
+      </div>
+      <div className='mb-6'>
+        <h3 className='text-black text-lg font-medium flex mb-2'>
+          <BookOpen classNames='w-6 mr-2' /> Self-Motivation
+        </h3>
+        <p className='text-black text-base'>
+          A self-taught developer who isn't afraid of a challenge and loves to
+          attack problems head on. Always learning new things.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/icons/Share.tsx
+++ src/components/icons/Share.tsx
@@ -9,9 +9,9 @@ function Share({ classNames }: ShareProps) {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       className={`feather feather-share-2 ${classNames}`}
     >
       <circle cx='18' cy='5' r='3'></circle>
