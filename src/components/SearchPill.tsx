8c584758b7e99067dc9bf081e8550e6622e1e157
Michael Scott Weber <mscottweber@gmail.com>
2025-03-19 17:50:51 -0500

    Changes color of search pill selcted outline

--- src/components/SearchPill.tsx
+++ src/components/SearchPill.tsx
@@ -18,7 +18,7 @@ function SearchPill({ name, selected, onClick }: SearchPillProps) {
     <button
       className={`border flex rounded-full py-1 px-2 text-xs text-black trabsition-all cursor-pointer hover:border-primary-500 hover:text-primary-500 ${
         selected
-          ? 'bg-gray-200 border-primary-500 text-primary-800'
+          ? 'bg-gray-200 border-secondary-500 text-primary-800'
           : 'border-gray-300'
       }`}
       onClick={handleOnClick}
