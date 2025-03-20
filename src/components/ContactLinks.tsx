1685937404359c9513b16b714d510ee90ee16cc2
Michael Scott Weber <mscottweber@gmail.com>
2025-03-20 18:15:02 -0500

    Fixes hover color on contact links

--- src/components/ContactLinks.tsx
+++ src/components/ContactLinks.tsx
@@ -4,32 +4,32 @@ import GitHub from './icons/GitHub';
 
 function ContactLinks() {
   return (
-    <div className='flex justify-center gap-4 text-black dark:text-gray-200'>
+    <div className='flex justify-center gap-4 text-black'>
       <a
         href='mailto:mscottweber@gmail.com'
         target='_blank'
         rel='noopener noreferrer'
-        className='flex flex-col items-center hover:text-primary-500 transition-all'
+        className='flex flex-col items-center hover:text-primary-500 dark:text-gray-200/80 transition-all'
       >
-        <Mail classNames='w-5 dark:text-gray-200/80' />
+        <Mail classNames='w-5' />
         <span className='text-xs'>Email</span>
       </a>
       <a
         href='https://www.linkedin.com/in/michaelscottweber/'
         target='_blank'
         rel='noopener noreferrer'
-        className='flex flex-col items-center text-black dark:text-gray-200 hover:text-primary-500 transition-all'
+        className='flex flex-col items-center text-black hover:text-primary-500 dark:text-gray-200/80 transition-all'
       >
-        <LinkedIn classNames='w-5 dark:text-gray-200/80' />
+        <LinkedIn classNames='w-5' />
         <span className='text-xs'>LinkedIn</span>
       </a>
       <a
         href='https://github.com/MichaelScottWeber'
         target='_blank'
         rel='noopener noreferrer'
-        className='flex flex-col items-center text-black dark:text-gray-200 hover:text-primary-500 transition-all'
+        className='flex flex-col items-center text-black dark:text-gray-200/80 hover:text-primary-500 transition-all'
       >
-        <GitHub classNames='w-5 dark:text-gray-200/80' />
+        <GitHub classNames='w-5' />
         <span className='text-xs'>GitHub</span>
       </a>
     </div>
