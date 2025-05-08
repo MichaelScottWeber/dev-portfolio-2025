db2d99bb85800d48bdf6e1f7238ad1aca5c1a580
Michael Scott Weber <mscottweber@gmail.com>
2025-05-08 15:29:37 -0500

    Adds begins building Loading component, with animations

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -10,24 +10,28 @@ function ProjectSection() {
     return <Project key={project.name} data={project} />;
   });
 
-  if (loading) return <Loading />;
-
-  if (error) return <Error error={error} />;
-
   return (
     <div className='py-5'>
       <h2 className='text-xl text-black dark:text-white font-medium mb-4 md:mb-7'>
         Projects
       </h2>
-      <span
-        aria-hidden='true'
-        className='text-xs text-gray-700 dark:text-gray-200/80 mb-4 inline-block md:hidden'
-      >
-        *Tap on a project for more info
-      </span>
-      <ul className='flex flex-col md:flex-row md:flex-wrap gap-5 gap-2 items-center justify-center'>
-        {renderProjectList}
-      </ul>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error error={error} />
+      ) : (
+        <>
+          <span
+            aria-hidden='true'
+            className='text-xs text-gray-700 dark:text-gray-200/80 mb-4 inline-block md:hidden'
+          >
+            *Tap on a project for more info
+          </span>
+          <ul className='flex flex-col md:flex-row md:flex-wrap gap-5 gap-2 items-center justify-center'>
+            {renderProjectList}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
